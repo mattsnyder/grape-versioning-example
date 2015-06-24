dcaae8b2f8d90f0124a966f85e73aad1ff85a9bc
Jerrod Blavos and Kane Baccigalupi <pair+jerrod+kane@usertesting.com>
2015-06-24 17:18:16 -0400

    added expectations on content type

--- spec/api/ping_spec.rb
+++ spec/api/ping_spec.rb
@@ -4,6 +4,7 @@ describe GrapeVersioningExample::Endpoints::PingV1 do
   it "GET /ping(version 1)" do
     with_api GrapeVersioningExample::Application do
       get_request(path: "/api/example/ping", head: { "accept" => 'application/vnd.mattsnyder.ping-v1+json'}) do |async|
+        expect( async.response_header['CONTENT_TYPE'] ).to eq("application/vnd.mattsnyder.ping-v1+json")
         expect( async.response ).to include("version 1")
       end
     end
@@ -14,6 +15,7 @@ describe GrapeVersioningExample::Endpoints::PingV2 do
   it "GET /ping(version 2)" do
     with_api GrapeVersioningExample::Application do
       get_request(path: "/api/example/ping", head: { "accept" => 'application/vnd.mattsnyder.ping-v2+json'}) do |async|
+        expect( async.response_header['CONTENT_TYPE'] ).to eq("application/vnd.mattsnyder.ping-v2+json")
         expect( async.response ).to include("version 2")
       end
     end
